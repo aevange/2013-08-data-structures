a373cd111a0f5032d3a1eaf09efa25e24322993d
Hack Reactor Students <github@hackreactor.com>
2013-08-07 15:45:54 -0700

    add functional(shared) stack

--- src/functional-shared/stack.js
+++ src/functional-shared/stack.js
@@ -1,3 +1,23 @@
 var makeStack = function(){
-  // Hey! Copy your code from src/functional/stack.js and paste it here
+  var stack = {};
+  stack._storage={};
+  stack._size=0;
+  _.extend(stack, methods);
+  return stack;
 };
+
+  var methods = {};
+
+  methods.push = function(value){
+    this._storage[this._size] = value;
+    this._size++;
+  };
+
+  methods.pop = function(){
+    this._size && this._size--;
+    return this._storage[this._size];
+  };
+
+  methods.size = function(){
+    return this._size;
+  };
