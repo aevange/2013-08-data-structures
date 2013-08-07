655b9f26ed98e7e3228be20579406078bb18333c
Hack Reactor Students <github@hackreactor.com>
2013-08-07 16:14:39 -0700

    Pseudoclassical stack passes all tests

--- src/pseudoclassical/stack.js
+++ src/pseudoclassical/stack.js
@@ -1,3 +1,18 @@
-var Stack = function() {
-  // Hey! Copy your code from src/prototypal/stack.js and paste it here
+var Stack = function(){
+  this._storage={};
+  this._size=0;
+};
+
+Stack.prototype.push = function(value){
+  this._storage[this._size] = value;
+  this._size++;
+};
+
+Stack.prototype.pop = function(){
+  this._size && this._size--;
+  return this._storage[this._size];
+};
+
+Stack.prototype.size = function(){
+  return this._size;
 };
