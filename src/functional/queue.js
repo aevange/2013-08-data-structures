e31dc081e3dbf590189d29deaedd91fa55505fc1
Hack Reactor Students <github@hackreactor.com>
2013-08-07 14:50:23 -0700

    queue passes all tests

--- src/functional/queue.js
+++ src/functional/queue.js
@@ -2,18 +2,30 @@ var makeQueue = function(){
   // Use an object with numeric keys to store values
   var storage = {};
 
-  var size; // Hint: set an initial value here
+  var size =0; // Hint: set an initial value here
+  var dequeuer = 0;
 
   // Implement the methods below
   var queue = {};
 
   queue.enqueue = function(value){
+    storage[size] = value;
+    size++;
   };
 
   queue.dequeue = function(){
+    return storage[dequeuer++];
   };
 
   queue.size = function(){
+    var length
+    if(size - dequeuer < 0) {
+     length = 0;
+   } else {
+    length = size - dequeuer;
+   }
+
+    return length;
   };
 
   return queue;
